d8f30204d73a52591a81f217bc7b83f40ebdda28
Astro <116896805+kikiIsSans@users.noreply.github.com>
2025-01-13 12:39:25 -0500

    Added Button Changes

--- src/building.js
+++ src/building.js
@@ -1,18 +1,47 @@
 class Building {
-    constructor(buildingName, baseCps, baseCost, buttonId){
+    constructor(buildingName, baseCps, baseCost, buttonId) {
         this.buildingName = buildingName;
         this.baseCps = baseCps / (1000 / tickRate);
+        this.baseCost = baseCost;
         this.cost = baseCost;
         this.buttonId = buttonId;
         this.amountOwned = 0;
         this.cps = 0;
-        this.visible = true; // change this to false
+        this.visible = false; // change this to false
     }
 
-    purchase(){
+    purchase() {
         score -= this.cost;
         this.amountOwned++;
         this.cps = this.baseCps * this.amountOwned;
         this.cost = Math.ceil(this.cost * 1.15 ** this.amountOwned);
+    }
+
+
+    buttonState(){
+    if (!this.visible) {
+        document.getElementById(this.buttonId).style.display = "none";
+        if (score >= this.baseCost) {
+            this.visible = true;
+            document.getElementById(this.buttonId).style.display = "initial";
+          }
+       }
+
+       if (score < this.cost) {
+        document.getElementById(this.buttonId).disabled = true;
+       } else {
+        document.getElementById(this.buttonId).disabled = false;
+       }
+
+       document.getElementById(this.buttonId).innerHTML = 
+       "Buy " + 
+       this.buildingName +
+        " (Cost: $" + 
+        Math.ceil(this.cost).toLocaleString() +
+        ") <br> Adds $" +
+        (this.baseCps * (1000 / tickRate)).toLocaleString() +
+        " Per Second <br> [Owned: " +
+        this.amountOwned +
+        "]";
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
 const tickRate = 1000 / 30;
 let score = 0;
 
-let survivors = new Building('Survivors', 0.1, 15, 'buySurvivors');
+let noob = new Building('Noob', 0.1, 15,'buyNoob');
+let elliot = new Building('Elliot', 1, 100,'buyElliot');
+let twoTime = new Building('Two Time', 3, 160,'buyTwoTime');
 
 function incScore() {
-    score += survivors.cps;
+    score += noob.cps;
+    score += elliot.cps;
+    score += twoTime.cps;
 }
 
 function scorePlusPlus(){
     score++;
 }
 
+function updateButton() {
+    noob.buttonState();
+    elliot.buttonState();
+    twoTime.buttonState();
+}
+
 function updatePage() {
     incScore();
+    updateButton();
     document.getElementById('score').innerHTML = "$" + Math.floor(score).toLocaleString();
 }
 
