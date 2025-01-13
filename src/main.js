cbe25b0661182a5dbcc7e9b7ec1285b950056f76
Astro <116896805+kikiIsSans@users.noreply.github.com>
2025-01-13 13:01:15 -0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -4,11 +4,16 @@ let score = 0;
 let noob = new Building('Noob', 0.1, 15,'buyNoob');
 let elliot = new Building('Elliot', 1, 100,'buyElliot');
 let twoTime = new Building('Two Time', 3, 160,'buyTwoTime');
+let guest = new Building('Guest', 5, 300,'buyGuest');
+let chance = new Building('Chance', 10, 1000,'buyChance');
+
 
 function incScore() {
     score += noob.cps;
     score += elliot.cps;
     score += twoTime.cps;
+    score += guest.cps;
+    score += chance.cps;
 }
 
 function scorePlusPlus(){
@@ -19,6 +24,9 @@ function updateButton() {
     noob.buttonState();
     elliot.buttonState();
     twoTime.buttonState();
+    guest.buttonState();
+    chance.buttonState();
+
 }
 
 function updatePage() {
